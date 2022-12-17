c1c812145a3fa408d13a586c9baab6704116e422
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2022-12-17 15:54:49 +0900

    인앱 csv 파일 추가, 불필요한 스크립트 제거, 인앱 슬롯 수정중

--- Assets/Scripts/UI/Store/ClonStoreSlot.cs
+++ Assets/Scripts/UI/Store/ClonStoreSlot.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Purchasing;
 
 public class ClonStoreSlot : MonoBehaviour
 {
+    [SerializeField] private IAPButton iapButton;
+
+    public void Init()
+    {
+        //iapButton.productId = "clon"
+    }
+
     public void OnPurchaseComplete()
     {
         Debug.Log("인앱 구매 성공");
