3701ce55860551904ea375554323ec497e30fa8d
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2023-02-11 01:42:50 +0900

    툴팁 관련 변경

--- Assets/Scripts/UI/System/UISystem.cs
+++ Assets/Scripts/UI/System/UISystem.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISystem : MonoBehaviour
 {
-    public static void Tooltip(string message, Vector3 position)
+    public static void Tooltip(string message, RectTransform rectTransform)
     {
-        var tooltip = Instantiate(Resources.Load<Tooltip>("Tooltip"), position, Quaternion.identity);
+        var tooltip = Instantiate(Resources.Load<Tooltip>("Tooltip"), rectTransform.root);
 
         tooltip.Init(message);
     }
 }
-     
