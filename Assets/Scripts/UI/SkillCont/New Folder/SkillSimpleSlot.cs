dad1bdc038f82158a767a2a41f26911763393e91
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2023-02-12 13:18:39 +0900

    툴팁 관련 수정

--- Assets/Scripts/UI/SkillCont/New Folder/SkillSimpleSlot.cs
+++ Assets/Scripts/UI/SkillCont/New Folder/SkillSimpleSlot.cs
@@ -11,7 +11,14 @@ public class SkillSimpleSlot : GameView
 
     public void Init(uint skillId)
     {
-        var desc = App.GameModel.PresetDataModel.ReturnData<Tooltip>(nameof(Tooltip), skillId).description;
+        var presetData = App.GameModel.PresetDataModel;
+
+        var statData = presetData.ReturnData<StatData>(nameof(StatData), skillId);
+        var desc = presetData.ReturnData<Tooltip>(nameof(Tooltip), skillId).description;
+
+        desc = desc.Replace("?0", "( " + statData.GetTotalSkillStatValue(Stat.SkillStatType.Damage).ToString() + " )");
+        desc = desc.Replace("?1", "( " + statData.GetTotalSkillStatValue(Stat.SkillStatType.DamageOverTime).ToString() + " )");
+        desc = desc.Replace("?2", "( " + statData.GetTotalSkillStatValue(Stat.SkillStatType.DamageOverTimeCount).ToString() + " )");
 
         button.onClick.RemoveAllListeners();
         button.onClick.AddListener(() => UISystem.TooltipBox(desc, transform.root));
