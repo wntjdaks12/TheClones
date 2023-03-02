e37e16e0abfdd062f6b380e269c32f7f5c177d48
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2023-03-02 22:15:24 +0900

    아이템 관련 주석 처리

--- Assets/Scripts/DB/DBModel.cs
+++ Assets/Scripts/DB/DBModel.cs
@@ -11,6 +11,10 @@ public class PlayerInfo
     public List<ClonInfo> cloneInofs = new List<ClonInfo>();
     [HideInInspector]
     public ReactiveCollection<ClonInfo> cloneInfosRP = new ReactiveCollection<ClonInfo>();
+
+    public List<ItemInfo> itemInfos = new List<ItemInfo>();
+    [HideInInspector]
+    public ReactiveCollection<ItemInfo> itemInfosRP = new ReactiveCollection<ItemInfo>();
 }
 
 [Serializable]
@@ -20,6 +24,12 @@ public class ClonInfo
     public uint[] skillId;
 }
 
+[Serializable]
+public class ItemInfo
+{
+    public uint itemId;
+    public int count;
+}
 
 [Serializable]
 public class AssetBundleInfo
