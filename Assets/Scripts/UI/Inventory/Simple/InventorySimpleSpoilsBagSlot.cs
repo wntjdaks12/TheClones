a3c97d73286652378e93dac78b33492c0917a7d5
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2023-03-03 23:46:06 +0900

    수정

--- Assets/Scripts/UI/Inventory/Simple/InventorySimpleSpoilsBagSlot.cs
+++ Assets/Scripts/UI/Inventory/Simple/InventorySimpleSpoilsBagSlot.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InventorySimpleSpoilsBagSlot : InventorySimpleSlot, IPollingScrollview
 {
+    [SerializeField] private TextMeshProUGUI countTMP;
+
     public void Init(int index)
     {
         var assetBundleManager = GameManager.Instance.GetManager<AssetBundleManager>();
         var playerManager = GameManager.Instance.GetManager<PlayerManager>();
 
-        var itemId = playerManager.PlayerInfo.itemInfos[index].itemId;
+        var itemInfo = playerManager.PlayerInfo.itemInfos[index];
 
-        var imageInfo = App.GameModel.PresetDataModel.ReturnData<ImageInfo>(nameof(ImageInfo), itemId);
+        var imageInfo = App.GameModel.PresetDataModel.ReturnData<ImageInfo>(nameof(ImageInfo), itemInfo.itemId);
+
+        countTMP.text = itemInfo.count.ToString();
 
         base.Init(index, assetBundleManager.AssetBundleInfo.texture.LoadAsset<Sprite>(imageInfo.Icon));
     }
