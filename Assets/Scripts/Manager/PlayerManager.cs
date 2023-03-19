c6646d270fce42f3da209a8c940c844a99b023c6
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2023-03-19 17:07:00 +0900

    임시 데이터 추가

--- Assets/Scripts/Manager/PlayerManager.cs
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -6,4 +6,11 @@ public class PlayerManager : GameManager
 {
     [SerializeField] private PlayerInfo playerInfo;
     public PlayerInfo PlayerInfo { get => playerInfo; set => playerInfo = value; }
+
+
+    // 나중에 삭제
+    public void Start()
+    {
+        playerInfo.itemInfos.Add(new ItemInfo { itemId = 120201, count = 1});
+    }
 }
