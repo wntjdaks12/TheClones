691abb7c277b01132c28e8a224b5eea2a6527e73
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2023-08-20 15:28:09 +0900

    디버그 삭제

--- Assets/JSMTest/SkillCollider.cs
+++ Assets/JSMTest/SkillCollider.cs
@@ -27,13 +27,11 @@ public class SkillCollider : MonoBehaviour
             var otherCharacter = otherCharacterObj.Entity as Character;
 
             if (otherCharacter.StateType == Actor.StateTypes.Death) return;
-            Debug.Log("a");
+
             for (int i = 0; i < skill.LayerName.Length; i++)
             {
-                Debug.Log("b");
                 if (skill.LayerName[i] == LayerMask.LayerToName(other.gameObject.layer))
                 {
-                    Debug.Log("c");
                     if (skill.SpawnCount > 0 && hitedOtherCharacterObjs.Count > skill.SpawnCount) return;
 
                     hitedOtherCharacterObjs.Add(otherCharacterObj);
@@ -41,7 +39,7 @@ public class SkillCollider : MonoBehaviour
                     triggerAsync = CoroutineHelper.StartCoroutine(skill.SkillStrategy.OnTrigger(otherCharacterObj.Entity, skill));
 
                     otherCharacterObj.Entity.OnDataRemove += (data) => CoroutineHelper.StopCoroutine(triggerAsync);
-                    Debug.Log("d");
+
                     if (skill.Id == 30003)
                     {
                         GameObject.Find("App").GetComponent<GameApplication>().GameController.GetComponent<ParticleController>().Spawn(nameof(Particle), 60004, Vector3.zero, otherCharacterObj.Entity);
